ab7d683f4f2b40f12c293cf4fa88f8e54542ffbe
Iztac <a-iztac@hotmail.com>
2022-09-07 16:05:17 -0500

    drt_liquidacion

--- src/FileCabinet/SuiteScripts/drt_custom_module/drt_mapid_cm.js
+++ src/FileCabinet/SuiteScripts/drt_custom_module/drt_mapid_cm.js
@@ -33,7 +33,116 @@ define(
             }
         }
 
+          const drt_liquidacion = () => {
+            let respuesta = {};
+            try {
+                const mapObj = {
+                    [runtime.EnvType.PRODUCTION]: {
+                        efectivo: 1,
+                        prepagoBanorte: 2,
+                        vale: 3,
+                        cortesia: 4,
+                        tarjetaCredito: 5,
+                        tarjetaDebito: 6,
+                        multiple: 7,
+                        prepagoTransferencia: 8,
+                        creditoCliente: 9,
+                        reposicion: 10,
+                        saldoAFavor: 11,
+                        consumoInterno: 12,
+                        prepagoBancomer: 13,
+                        prepagoHSBC: 14,
+                        prepagoBanamex: 15,
+                        prepagoSantander: 16,
+                        prepagoScotian: 17,
+                        bonificacion: 18,
+                        ticketCard: 19,
+                        chequeBancomer: 20,
+                        recirculacion: 21,
+                        cancelado: 22,
+                        relleno: 23,
+                        transferencia: 24,
+                        traspaso: 25,
+                        chequeSantander: 26,
+                        chequeScotian: 27,
+                        chequeHSBC: 28,
+                        chequeBanamex: 29,
+                        chequeBanorte: 30,
+                        tarjetaCreditoBancomer: 31,
+                        tarjetaCreditoHSBC: 32,
+                        tarjetaCreditoBanamex: 33,
+                        tarjetaDebitoBanamex: 34,
+                        tarjetaDebitoBancomer: 35,
+                        tarjetaDebitoHSBC: 36,
+                        publicoGeneral: 27041,
+                        urlPago: "https://5298967.app.netsuite.com/app/common/custom/custrecordentry.nl?rectype=587&id=",
+                        estatusRecibido: 2,
+                        cilindro10: 4210,
+                        cilindro20: 4211,
+                        cilindro30: 4212,
+                        cilindro45: 4213,
+                        envase10: 4206,
+                        envase20: 4207,
+                        envase30: 4208,
+                        envase45: 4209,
+                        gasLP: 4216,
+                    },
+                    [runtime.EnvType.SANDBOX]: {
+                        efectivo: 1,
+                        prepagoBanorte: 2,
+                        vale: 3,
+                        cortesia: 4,
+                        tarjetaCredito: 5,
+                        tarjetaDebito: 6,
+                        multiple: 7,
+                        prepagoTransferencia: 8,
+                        creditoCliente: 9,
+                        reposicion: 10,
+                        saldoAFavor: 11,
+                        consumoInterno: 12,
+                        prepagoBancomer: 13,
+                        prepagoHSBC: 14,
+                        prepagoBanamex: 15,
+                        prepagoSantander: 16,
+                        prepagoScotian: 17,
+                        bonificacion: 18,
+                        ticketCard: 19,
+                        chequeBancomer: 20,
+                        recirculacion: 21,
+                        cancelado: 22,
+                        relleno: 23,
+                        transferencia: 24,
+                        traspaso: 25,
+                        chequeSantander: 26,
+                        chequeScotian: 27,
+                        chequeHSBC: 28,
+                        chequeBanamex: 29,
+                        chequeBanorte: 30,
+                        publicoGeneral: 14508,
+                        urlPago: "https://5298967-sb1.app.netsuite.com/app/common/custom/custrecordentry.nl?rectype=503&id=",
+                        estatusRecibido: 2,
+                        cilindro10: 4094,
+                        cilindro20: 4095,
+                        cilindro30: 4096,
+                        cilindro45: 4602,
+                        envase10: 4097,
+                        envase20: 4099,
+                        envase30: 4098,
+                        envase45: 4604,
+                        gasLP: 4088,
+                    }
+                }
+                respuesta = mapObj[runtime.envType];
+            } catch (error) {
+                log.error(`error drt_liquidacion`, error);
+            } finally {
+                log.debug(`respuesta drt_liquidacion ${runtime.envType}`, respuesta);
+                return respuesta;
+            }
+        }
+
         return {
+            drt_liquidacion,
             ptg_mr_update_opportunities
         };
 
