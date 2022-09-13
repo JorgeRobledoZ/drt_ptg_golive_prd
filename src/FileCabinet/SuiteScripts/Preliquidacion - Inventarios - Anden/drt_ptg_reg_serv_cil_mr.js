d6f6d3fa23f03b2f7dd4926071db903f51804f50
Jose Fernandez <josefernandeztiburcio@gmail.com>
2022-09-13 17:42:12 -0500

    .

--- src/FileCabinet/SuiteScripts/Preliquidacion - Inventarios - Anden/drt_ptg_reg_serv_cil_mr.js
+++ src/FileCabinet/SuiteScripts/Preliquidacion - Inventarios - Anden/drt_ptg_reg_serv_cil_mr.js
@@ -63,6 +63,7 @@
             });
             valoresProceso.custrecord_ptg_etapa_reg_serv_cil = 3;
         } finally {
+            
             var registroCilindros = record.submitFields({
                 type: "customrecord_ptg_registro_servicios_cili",
                 id: idRegistro,
@@ -84,6 +85,10 @@
                 title: 'context map',
                 details: JSON.stringify(context)
             });
+            var idRegistro = runtime.getCurrentScript().getParameter({
+                name: 'custscript_drt_ptg_id_registro_serv_cil'
+            }) || '';
+            log.debug("idRegistro", idRegistro);
 
             var objValue = JSON.parse(context.value);
             var idRegistroCilindroLinea = objValue.id;
@@ -92,6 +97,7 @@
             var objPagosOportunidad = {};
             var regServCilUpdate = {};
             var oportunidadArray = [];
+            var valoresProceso = {};
             var publicoGeneral = 0;
             var cilindro10 = 0;
             var cilindro20 = 0;
@@ -144,7 +150,8 @@
                 unidad20 = objMap.unidad20;
                 unidad30 = objMap.unidad30;
                 unidad45 = objMap.unidad45;
-                condretado = objMap.condretado;
+                //condretado = objMap.condretado;
+                condretado = 13;
                 entregado = objMap.entregado;
                 ventaLitro = objMap.ventaLitro;
                 traspasoId = objMap.traspasoId;
@@ -494,6 +501,7 @@
                 }
             }
             
+            valoresProceso.custrecord_ptg_etapa_reg_serv_cil = 1;
 
             context.write({
                 key: recOportunidadIdSaved,
@@ -501,10 +509,25 @@
             });
                
         } catch (error) {
+            valoresProceso.custrecord_ptg_etapa_reg_serv_cil = 3;
+            var registroCilindros = record.submitFields({
+                type: "customrecord_ptg_registro_servicios_cili",
+                id: idRegistro,
+                values: valoresProceso
+            });
+            log.debug("registro error Cilindros map", registroCilindros);
             log.error({
                 title: 'error map',
                 details: JSON.stringify(error)
             });
+        } finally {
+            
+            var registroCilindros = record.submitFields({
+                type: "customrecord_ptg_registro_servicios_cili",
+                id: idRegistro,
+                values: valoresProceso
+            });
+            log.debug("registro Cilindros map", registroCilindros);
         }
     }
 
@@ -561,7 +584,7 @@
     return {
         getInputData: getInputData,
         map: map,
-        reduce: reduce,
-        summarize: summarize
+        //reduce: reduce,
+        //summarize: summarize
     }
 });
