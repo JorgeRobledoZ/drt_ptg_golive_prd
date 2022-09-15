58b8e8cd22b5eea97e0a25c7943125b0271b783f
Iztac <a-iztac@hotmail.com>
2022-09-14 20:22:17 -0500

    customrecord_ptg_preliqestcarburacion_
    se ajusta el desarrollo para definir JSON de eliminacion donde se plasman filtros y dependencias de registros

--- src/FileCabinet/SuiteScripts/drt_custom_module/drt_ptg_delete_record_cm.js
+++ src/FileCabinet/SuiteScripts/drt_custom_module/drt_ptg_delete_record_cm.js
@@ -47,6 +47,7 @@ define([
                                     field: "custrecord_ptg_oportunidad",
                                 },
                             ],
+                            filters: ["AND", ["custrecord_ptg_origen", search.Operator.IS, 'F']]
                         },
                         {
                             record: "customrecord_ptg_detalledeventas_",
@@ -63,6 +64,79 @@ define([
                         {
                             record: "customrecord_ptg_oportunidad_facturar",
                             field: "custrecord_ptg_preliq_cilindros"
+                        },
+                    ],
+                    customrecord_ptg_preliqestcarburacion_: [
+                        {
+                            record: "customrecord_ptg_det_tipo_pago_est_car_",
+                            field: "custrecord_ptg_detalle_pago_carb_"
+                        },
+                        {
+                            record: "customrecord_ptg_detallegas_",
+                            field: "custrecord_ptg_detalle_cil"
+                        },
+                        {
+                            record: "customrecord_ptg_totalesxtipopago_est_ca",
+                            field: "custrecord_ptg_envases_por_tipopago_"
+                        },
+                        {
+                            record: "customrecord_ptg_totalestipopago_detalle",
+                            field: "custrecord_ptg_tipopagoresumen_"
+                        },
+                        {
+                            record: "customrecord_ptg_oportunidades_",
+                            field: "custrecord_ptg_rel_op_preliq_"
+                        },
+                        {
+                            record: "customrecord_ptg_detalle_despachador_",
+                            field: "custrecord_ptg_detallecrburacion_", //Checar
+                            field2: "custrecord_ptg_oportunidad_carburacion",
+                            opportunity: [
+                                {
+                                    record: "customrecord_ptg_pagos",
+                                    field: "custrecord_ptg_oportunidad_pagos",
+                                    filters: ["AND", ["custrecord_ptg_oportunidad_pagos.custbody_ptg_servicio_id", search.Operator.ISEMPTY, '']]
+                                },
+                                {
+                                    record: "customrecord_ptg_pagos_oportunidad",
+                                    field: "custrecord_ptg_oportunidad",
+                                    filters: ["AND", ["custrecord_ptg_oportunidad.custbody_ptg_servicio_id", search.Operator.ISEMPTY, '']]
+                                },
+                            ],
+                        },
+                        {
+                            record: "customrecord_ptg_det_gas_tipo_pago_",
+                            field: "custrecord_ptg_detgas_tipo_pago_",
+                            field2: "custrecord_ptg_id_oportunidad_gas",
+                            opportunity: [
+                                {
+                                    record: "customrecord_ptg_pagos",
+                                    field: "custrecord_ptg_oportunidad_pagos",
+                                    filters: ["AND", ["custrecord_ptg_oportunidad_pagos.custbody_ptg_servicio_id", search.Operator.ISEMPTY, '']]
+                                },
+                                {
+                                    record: "customrecord_ptg_pagos_oportunidad",
+                                    field: "custrecord_ptg_oportunidad",
+                                    filters: ["AND", ["custrecord_ptg_oportunidad.custbody_ptg_servicio_id", search.Operator.ISEMPTY, '']]
+                                },
+                            ],
+                        },
+                        {
+                            record: "customrecord_ptg_detalleenv_est_carb_",
+                            field: "custrecord_ptg_envdetallecarb_",
+                            field2: "custrecord_ptg_id_oportunidad_envases",
+                            opportunity: [
+                                {
+                                    record: "customrecord_ptg_pagos",
+                                    field: "custrecord_ptg_oportunidad_pagos",
+                                    filters: ["AND", ["custrecord_ptg_oportunidad_pagos.custbody_ptg_servicio_id", search.Operator.ISEMPTY, '']]
+                                },
+                                {
+                                    record: "customrecord_ptg_pagos_oportunidad",
+                                    field: "custrecord_ptg_oportunidad",
+                                    filters: ["AND", ["custrecord_ptg_oportunidad.custbody_ptg_servicio_id", search.Operator.ISEMPTY, '']]
+                                },
+                            ],
                         },
                     ]
                 };
@@ -112,6 +186,7 @@ define([
                     if (
                         record_related.success
                     ) {
+                        const objOpportuniti = {};
                         record_related.data.forEach(element => {
                             const columsCustomRecord = [
                                 {
@@ -125,14 +200,20 @@ define([
                                     name: element.field2,
                                 });
                             }
-
+                            let filters = [
+                                ["isinactive", search.Operator.IS, "F"],
+                                "AND",
+                                [element.field, search.Operator.IS, record_id]
+                            ];
+                            if (
+                                !!element.filters &&
+                                element.filters.length > 0
+                            ) {
+                                filters = filters.concat(element.filters);
+                            }
                             const dataRecord = drt_ptg_address_cm.arraySearchRecord(
                                 element.record,
-                                [
-                                    ["isinactive", search.Operator.IS, "F"],
-                                    "AND",
-                                    [element.field, search.Operator.IS, record_id]
-                                ],
+                                filters,
                                 columsCustomRecord
                             );
                             respuesta.data = respuesta.data.concat(dataRecord);
@@ -142,50 +223,54 @@ define([
                                 !!element.field2 &&
                                 !!element.opportunity &&
                                 element.opportunity.length > 0
-
                             ) {
                                 const arrayOpportunity = dataRecord.map(x => x[element.field2]);
                                 log.debug(`arrayOpportunity`, arrayOpportunity);
-                                const concatOpportunity = dataRecord.map(x => {
-                                    return {
-                                        id: x[element.field2],
-                                        recordType: record.Type.OPPORTUNITY,
-                                    }
-                                });
-
-                                log.debug(`concatOpportunity`, concatOpportunity);
-
-                                respuesta.data = concatOpportunity.concat(respuesta.data);
                                 element.opportunity.forEach(eopp => {
+                                    let filters2 = [
+                                        ["isinactive", search.Operator.IS, "F"],
+                                        "AND",
+                                        [eopp.field, search.Operator.ANYOF, arrayOpportunity]
+                                    ];
+                                    if (
+                                        !!eopp.filters &&
+                                        eopp.filters.length > 0
+                                    ) {
+                                        filters2 = filters2.concat(eopp.filters);
+                                    }
+
                                     const dataRecord2 = drt_ptg_address_cm.arraySearchRecord(
                                         eopp.record,
-                                        [
-                                            ["isinactive", search.Operator.IS, "F"],
-                                            "AND",
-                                            [eopp.field, search.Operator.ANYOF, arrayOpportunity]
-                                        ],
+                                        filters2,
                                         [
                                             {
                                                 name: eopp.field,
                                             }
                                         ]
                                     );
-                                    log.debug(eopp.record, [
-                                        ["isinactive", search.Operator.IS, "F"],
-                                        "AND",
-                                        [eopp.field, search.Operator.ANYOF, arrayOpportunity]
-                                    ]);
-                                    log.debug(eopp.record, [
-                                        {
-                                            name: eopp.field,
+                                    log.debug(`dataRecord2`, dataRecord2);
+                                    dataRecord2.forEach(x => {
+                                        if (
+                                            !objOpportuniti[x[eopp.field]]
+                                        ) {
+                                            objOpportuniti[x[eopp.field]] = 0;
                                         }
-                                    ]);
-                                    log.debug(eopp.record, dataRecord2);
+                                        objOpportuniti[x[eopp.field]]++;
+                                    });
                                     respuesta.data = dataRecord2.concat(respuesta.data);
                                 });
-
                             }
                         });
+                        log.debug(`objOpportuniti`, objOpportuniti);
+                        const concatOpportunity = Object.keys(objOpportuniti).map(x => {
+                            return {
+                                id: x,
+                                recordType: record.Type.OPPORTUNITY,
+                            }
+                        });
+                        log.debug(`concatOpportunity`, concatOpportunity);
+                        respuesta.data = respuesta.data.concat(concatOpportunity);
+
                         respuesta.data.push({
                             id: record_id,
                             recordType: record_type,
@@ -200,7 +285,65 @@ define([
                 return respuesta;
             }
         }
+        const keyRecordType = (param_record_type, param_bloque) => {
+            const recordTypeNs = [
+                "customrecord_ptg_pagos_oportunidad",
+                "customrecord_ptg_detalledeventas_",
+                "customrecord_ptg_registrodedotacion_cil_",
+                "customrecord_ptg_ventadeenvases",
+                "customrecord_ptg_registrooportunidad_",
+                "customrecord_ptg_regitrodemovs_",
+                "customrecord_ptg_detalle_resumen_",
+                "customrecord_ptg_oportunidad_facturar",
+                "customrecord_ptg_det_tipo_pago_est_car_",
+                "customrecord_ptg_detallegas_",
+                "customrecord_ptg_totalesxtipopago_est_ca",
+                "customrecord_ptg_totalestipopago_detalle",
+                "customrecord_ptg_oportunidades_",
+                "customrecord_ptg_detalle_despachador_",
+                "customrecord_ptg_det_gas_tipo_pago_",
+                "customrecord_ptg_detalleenv_est_carb_",
+                "customrecord_ptg_pagos",
+                record.Type.OPPORTUNITY,
+                "customrecord_ptg_preliquicilndros_",
+                "customrecord_ptg_preliqestcarburacion_",
+            ];
+            const arrayOrden = [
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "J",
+                "K",
+                "L",
+                "M",
+                "N",
+                "O",
+                "P",
+                "Q",
+                "R",
+                "S",
+                "T",
+                "U",
+            ];
+            if (
+                param_record_type
+            ) {
+                return arrayOrden[recordTypeNs.indexOf(param_record_type)];
+            } else if (
+                param_bloque
+            ) {
+                return recordTypeNs[arrayOrden.indexOf(param_bloque)];
+            }
+            //log.debug(`respuesta keyRecordType`, respuesta);
+        }
         return {
+            keyRecordType,
             getAllRecord,
             readParameter,
             recordRelate
