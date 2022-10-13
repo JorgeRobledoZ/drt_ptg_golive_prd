83913e2afc94e9651e794d2b82a2848fc7f93e7d
Iztac <a-iztac@hotmail.com>
2022-10-13 11:28:41 -0500

    formato js

--- src/FileCabinet/SuiteScripts/ptg_compras/ptg_create_pg_ue.js
+++ src/FileCabinet/SuiteScripts/ptg_compras/ptg_create_pg_ue.js
@@ -2,24 +2,24 @@
  *@NApiVersion 2.1
  *@NScriptType UserEventScript
  */
-define(["SuiteScripts/drt_custom_module/drt_mapid_cm","N/record", "N/search"], function (drt_mapid_cm, record, search) {
+define(["SuiteScripts/drt_custom_module/drt_mapid_cm", "N/record", "N/search"], function (drt_mapid_cm, record, search) {
 
     function afterSubmit(context) {
         try {
             if (context.type == context.UserEventType.EDIT) {
 
-                var  currentRecord2 = context.newRecord;
+                var currentRecord2 = context.newRecord;
 
                 var lineas = currentRecord2.getLineCount({
                     sublistId: 'item'
                 });
 
                 var objRecepcion = {};
-                var arrayRecepcion =[];
+                var arrayRecepcion = [];
 
                 let ubicacion_desvio_planta_receipt = 0;
-                var objMap=drt_mapid_cm.drt_compras();
-                if (Object.keys(objMap).length>0) {
+                var objMap = drt_mapid_cm.drt_compras();
+                if (Object.keys(objMap).length > 0) {
                     ubicacion_desvio_planta_receipt = objMap.ubicacion_desvio_planta_receipt;
                 }
 
@@ -65,24 +65,23 @@ define(["SuiteScripts/drt_custom_module/drt_mapid_cm","N/record", "N/search"], f
                     });
 
 
-
                     log.audit('planta_desvio', planta_desvio);
 
                     log.audit('pg_asignado', pg_asignado);
 
                     log.audit('proceso', proceso);
 
-                    if(!proceso){
-                        if(planta_desvio && pg_asignado){
+                    if (!proceso) {
+                        if (planta_desvio && pg_asignado) {
                             objRecepcion = {
-                                id_po:  context.newRecord.id,
+                                id_po: context.newRecord.id,
                                 planta_desvio: planta_desvio,
                                 cantidad: quantity,
                                 articulo: item,
                                 pg_asignado: pg_asignado,
                                 subsidiaria: subsidiaria
                             }
-    
+
                             arrayRecepcion.push(objRecepcion)
                         }
                     }
@@ -91,7 +90,7 @@ define(["SuiteScripts/drt_custom_module/drt_mapid_cm","N/record", "N/search"], f
                     if (pg_asignado) {
                         log.audit('inicia proceso', '.............................0%');
 
-                        if(pg_asignado == 'DEFAULT'){
+                        if (pg_asignado == 'DEFAULT') {
                             log.audit('alerta', 'No se creara el pg por ser un PG de Default')
                         } else {
                             var customrecord_ptg_compras_SearchObj = search.create({
@@ -108,39 +107,39 @@ define(["SuiteScripts/drt_custom_module/drt_mapid_cm","N/record", "N/search"], f
                                 ]
                             });
                             var searchResultCount = customrecord_ptg_compras_SearchObj.run().getRange(0, 1);
-    
-                            if(searchResultCount.length > 0) {
+
+                            if (searchResultCount.length > 0) {
                                 var id = searchResultCount[0].getValue({
                                     name: "internalid"
                                 })
-                               log.audit('error', 'ya se creo un registro con este nombre se procede a actuializar el registro');
-    
-                               var pgComnprasLoad = record.load({
+                                log.audit('error', 'ya se creo un registro con este nombre se procede a actuializar el registro');
+
+                                var pgComnprasLoad = record.load({
                                     type: 'customrecord_ptg_compras_',
                                     id: id,
                                     isDynamic: true,
-                               });
-    
-                               pgComnprasLoad.setValue({
+                                });
+
+                                pgComnprasLoad.setValue({
                                     fieldId: 'custrecord_ptg_pg_en_uso',
                                     value: true
                                 });
-    
+
                                 var savePgComprasLoad = pgComnprasLoad.save();
                                 log.audit('savePgComprasLoad', savePgComprasLoad);
                             } else {
-                            var pgCompras = record.create({
-                                type: 'customrecord_ptg_compras_',
-                                isDynamic: 'true'
-                            });
-    
-                            pgCompras.setValue({
-                                fieldId: 'name',
-                                value: pg_asignado
-                            });
-    
-                            var idPgComras = pgCompras.save();
-                            log.audit('idPgCompras', idPgComras);
+                                var pgCompras = record.create({
+                                    type: 'customrecord_ptg_compras_',
+                                    isDynamic: 'true'
+                                });
+
+                                pgCompras.setValue({
+                                    fieldId: 'name',
+                                    value: pg_asignado
+                                });
+
+                                var idPgComras = pgCompras.save();
+                                log.audit('idPgCompras', idPgComras);
                             }
                         }
                     }
@@ -149,8 +148,8 @@ define(["SuiteScripts/drt_custom_module/drt_mapid_cm","N/record", "N/search"], f
                 log.audit('array', arrayRecepcion)
 
                 /*************Recepcion Desvia a Planta******************* */
-                if(arrayRecepcion.length > 0 ){
-                    for(var r in arrayRecepcion){
+                if (arrayRecepcion.length > 0) {
+                    for (var r in arrayRecepcion) {
                         log.audit('arratR', arrayRecepcion[r])
                         var recepcionDesvio = record.transform({
                             fromType: record.Type.PURCHASE_ORDER,
@@ -158,58 +157,61 @@ define(["SuiteScripts/drt_custom_module/drt_mapid_cm","N/record", "N/search"], f
                             toType: record.Type.ITEM_RECEIPT,
                             isDynamic: true,
                         });
-    
+
                         log.audit('recepcionDesvio', recepcionDesvio)
 
                         var customrecord_ptg_mapeo_almacenes_virt_SearchObj = search.create({
                             type: "customrecord_ptg_mapeo_almacenes_virt_",
                             filters:
-                            [
-                               ["custrecord_ptg_planta_mapeo_","anyof", location]
-                            ],
+                                [
+                                    ["custrecord_ptg_planta_mapeo_", "anyof", location]
+                                ],
                             columns:
-                            [
-                               search.createColumn({name: "custrecord_ptg_almacen_virtual_", label: "PTG - Almacén Virtual"})
-                            ]
-                         });
-                         var searchMaperoAlmacen = customrecord_ptg_mapeo_almacenes_virt_SearchObj.run().getRange(0, 1);
-                         log.audit('centroE', searchMaperoAlmacen.length);
-                         if (searchMaperoAlmacen.length > 0) {
-                               var idAlmacenVirtual = searchMaperoAlmacen[0].getValue({
-                                 name: 'custrecord_ptg_almacen_virtual_'
-                               })
-                            }
-                          log.audit('idAlmacenVirtual', idAlmacenVirtual)
-    
+                                [
+                                    search.createColumn({
+                                        name: "custrecord_ptg_almacen_virtual_",
+                                        label: "PTG - Almacén Virtual"
+                                    })
+                                ]
+                        });
+                        var searchMaperoAlmacen = customrecord_ptg_mapeo_almacenes_virt_SearchObj.run().getRange(0, 1);
+                        log.audit('centroE', searchMaperoAlmacen.length);
+                        if (searchMaperoAlmacen.length > 0) {
+                            var idAlmacenVirtual = searchMaperoAlmacen[0].getValue({
+                                name: 'custrecord_ptg_almacen_virtual_'
+                            })
+                        }
+                        log.audit('idAlmacenVirtual', idAlmacenVirtual)
+
                         recepcionDesvio.setValue({
-                            fieldId: 'location', 
+                            fieldId: 'location',
                             value: idAlmacenVirtual
                         })
-    
+
                         var lineasRecepcion = recepcionDesvio.getLineCount({
                             sublistId: 'item'
                         });
-    
-                        for(var t = 0; t < lineasRecepcion; t++){
+
+                        for (var t = 0; t < lineasRecepcion; t++) {
                             recepcionDesvio.selectLine({
                                 sublistId: 'item',
                                 line: t
                             });
-    
+
                             var pgAsignado = recepcionDesvio.getCurrentSublistValue({
                                 sublistId: 'item',
                                 fieldId: 'custcol2disp'
                             });
-    
-                            if(pgAsignado == arrayRecepcion[r]['pg_asignado']){
-    
+
+                            if (pgAsignado == arrayRecepcion[r]['pg_asignado']) {
+
                                 recepcionDesvio.setCurrentSublistValue({
                                     sublistId: 'item',
                                     fieldId: 'location',
                                     value: idAlmacenVirtual,
                                     line: t
                                 });
-    
+
                                 recepcionDesvio.setCurrentSublistValue({
                                     sublistId: 'item',
                                     fieldId: 'quantity',
@@ -223,43 +225,43 @@ define(["SuiteScripts/drt_custom_module/drt_mapid_cm","N/record", "N/search"], f
                                     value: false
                                 });
                             }
-    
+
                             recepcionDesvio.commitLine({
                                 sublistId: 'item'
                             });
                         }
-    
+
                         var idRecepcion = recepcionDesvio.save();
                         log.audit('idRecepcion', idRecepcion);
-                        
+
                         /******Seteo de campos ****** */
-    
-                        var  registro = record.load({
+
+                        var registro = record.load({
                             type: context.newRecord.type,
                             id: context.newRecord.id,
                             isDynamic: false
                         });
-                        var  lineasPo = registro.getLineCount('item');
-                            
-                        for(var y = 0; y < lineasPo; y++){
+                        var lineasPo = registro.getLineCount('item');
+
+                        for (var y = 0; y < lineasPo; y++) {
                             var poF = registro.getSublistValue({
                                 sublistId: 'item',
                                 fieldId: 'custcol2',
                                 line: y
                             });
-    
+
                             log.audit('poF', poF)
-                            if(poF){
+                            if (poF) {
                                 if (poF == arrayRecepcion[r]['pg_asignado']) {
                                     log.audit('emtro', '123456789')
-        
+
                                     registro.setSublistValue({
                                         sublistId: 'item',
                                         fieldId: 'custcol_ptg_recepcion_',
                                         value: idRecepcion,
                                         line: y
                                     });
-    
+
                                     registro.setSublistValue({
                                         sublistId: 'item',
                                         fieldId: 'custcol_ptg_proceso_terminado',
@@ -269,9 +271,9 @@ define(["SuiteScripts/drt_custom_module/drt_mapid_cm","N/record", "N/search"], f
                                 }
                             }
                         }
-    
+
                         var idPoCammbio = registro.save();
-                        log.audit('idPoCammbio',idPoCammbio)
+                        log.audit('idPoCammbio', idPoCammbio)
                     }
 
                 }
