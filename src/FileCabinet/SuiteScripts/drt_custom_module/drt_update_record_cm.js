e19782f41826f0f3ab7a95cc15b4017632e04f27
Jose Fernandez <josefernandeztiburcio@gmail.com>
2022-10-14 10:59:35 -0500

    Cambio a redirect.toSuitelet
    Se agrega el modulo redirect y se hará un redireccionamiento para que se ejecuten los ue dependientes de la transacción

--- src/FileCabinet/SuiteScripts/drt_custom_module/drt_update_record_cm.js
+++ src/FileCabinet/SuiteScripts/drt_custom_module/drt_update_record_cm.js
@@ -4,12 +4,14 @@
 define([
         'N/url',
         'N/https',
-        'N/record'
+        'N/record',
+        'N/redirect'
     ],
     (
         url,
         https,
-        record
+        record,
+        redirect
     ) => {
         /**
          * Realiza una peticion htts.post al suitelet customscript_drt_update_record_sl para editar y guardar cuaqlquier registro.
@@ -25,7 +27,7 @@ define([
                     !!param_id &&
                     !!param_type
                 ) {
-                    const scheme = 'https://';
+                    /*const scheme = 'https://';
                     const host = url.resolveDomain({
                         hostType: url.HostType.APPLICATION
                     });
@@ -42,7 +44,19 @@ define([
                             custscript_drt_update_record_sl_type: param_type,
                             custscript_drt_update_record_sl_id: param_id,
                         }
+                    });*/
+
+                    const parametros = {
+                        custscript_drt_update_record_sl_type: param_type,
+                        custscript_drt_update_record_sl_id: param_id,
+                    }
+
+                    var redirectToStl = redirect.toSuitelet({
+                        scriptId: "customscript_drt_update_record_sl",
+                        deploymentId: "customdeploy_drt_update_record_sl",
+                        parameters: parametros
                     });
+                    log.audit("redirectToStl", redirectToStl);
                 }
             } catch (error) {
                 log.error(`error requestSuitelet ${param_id} ${param_type}`, error);
